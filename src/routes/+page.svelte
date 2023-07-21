cebf3e161c255da57bba4a40f616cd2835177410
MasturFyc <mastur.st12@gmail.com>
2023-07-21 11:43:03 +0700

    office

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+
+type iMember = {
+  id: number;
+  name: string;
+  created_at: string;
+  address?: string;
+  phone?: string;
+  is_active: boolean;
+  type_id: number;
+}
+
+type iCustomer = iMember & {
+  unit_id: number;
+  unit_name?: string;
+  paket_id: number;
+  paket_name?: string;
+}
+
+const endpoint = '/customer/index';
+
+let customers: iCustomer[] = []
+
+const loadCustomer = async (callback: (e: iCustomer[]) => void) => {
+  const data = await fetch(endpoint);
+  callback(await data.json())
+}
+
+onMount(() => {
+  loadCustomer((e: iCustomer[]) => {
+    customers = e;
+  })
+})
+
+</script>
 <h1>Welcome to Fine Young Canibals</h1>
 <p>Visit <a href="https://sapulidi.site">sapulidi.site</a> to read the documentation</p>
+<ul>
+{#each customers as c (c.id)}
+  <li>{c.name}
+{/each}
+</ul>
