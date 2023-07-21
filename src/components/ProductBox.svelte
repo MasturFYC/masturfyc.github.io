58e147f99993421d97cb551753586966f4504b23
MasturFyc <mastur.st12@gmail.com>
2023-07-21 15:40:55 +0700

    office

---
+++ src/components/ProductBox.svelte
@@ -0,0 +1,50 @@
+<div class="box">
+	<slot name="picture">
+		<div class="div-image" />
+	</slot>
+	<div class="p-16">
+		<div class="flex-row">
+			<span class="product-name flex-1"><slot name="name" /></span>
+      <span class="product-price"><slot name="price" /></span>&nbsp;{'/'}&nbsp;<slot name="unit" />
+		</div>
+		<div>
+      <slot name="variant" />
+		</div>
+	</div>
+</div>
+
+<style>
+	.div-image {
+		background-image: url('https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg');
+		background-position: center;
+		background-size: cover;
+		background-repeat: no-repeat;
+		min-height: 120px;
+	}
+
+	.product-name {
+		font-size: larger;
+	}
+	.product-price {
+		font-weight: 700;
+	}
+
+	.box {
+		border: 1px solid #b9b9b9;
+		border-radius: 3px;
+		min-width: 280px;
+		flex: 1;
+    box-shadow: 1px 1px 3px #cecece;
+	}
+	.p-16 {
+		padding: 16px;
+	}
+  .flex-row {
+    display: flex;
+    flex-direction: row;
+    align-items: baseline;
+  }
+  .flex-1 {
+    flex: 1;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,77 @@
 <script lang="ts">
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
+	import ProductBox from '../components/ProductBox.svelte';
 
+	type iProduct = {
+		id: number;
+		category_id: number;
+		parent_id: number;
+		name: string;
+		barcode: string;
+		unit: string;
+		content: number;
+		hpp: number;
+		ppn: number;
+		is_active: boolean;
+		margin: number;
+		price: number;
+		is_for_sale: boolean;
+		is_inventory: boolean;
+		variant_name?: string;
+		descriptions?: string;
+		category_name?: string;
+	};
 
-type iMember = {
-  id: number;
-  name: string;
-  created_at: string;
-  address?: string;
-  phone?: string;
-  is_active: boolean;
-  type_id: number;
-}
+	const endpoint = 'https://sapulidi.site/api/v1/product-list';
 
-type iCustomer = iMember & {
-  unit_id: number;
-  unit_name?: string;
-  paket_id: number;
-  paket_name?: string;
-}
+	let products: iProduct[] = [];
+	let page = 0;
+	let limit = 5;
+  let hashMore = false;
 
-const endpoint = '/customer/index';
+	const loadCustomer = async (page = 0, limit = 5) => {
+		const d = await fetch(`${endpoint}/${page}/${limit}`);
+    const p = await d.json() ?? [];
+    hashMore = p.length < limit;
+    products = [...products, ...p];
+	};
 
-let customers: iCustomer[] = []
-
-const loadCustomer = async (callback: (e: iCustomer[]) => void) => {
-  const data = await fetch(endpoint);
-  callback(await data.json())
-}
-
-onMount(() => {
-  loadCustomer((e: iCustomer[]) => {
-    customers = e;
-  })
-})
-
+	$: loadCustomer(page, limit);
 </script>
+
 <h1>Welcome to Fine Young Canibals</h1>
 <p>Visit <a href="https://sapulidi.site">sapulidi.site</a> to read the documentation</p>
-<ul>
-{#each customers as c (c.id)}
-  <li>{c.name}
-{/each}
-</ul>
+<div class="div-product">
+	{#each products as c (c.id)}
+		<ProductBox>
+			<span slot="name">{c.name}</span>
+      <span slot="variant">{c.variant_name ?? ''} ({c.category_name})</span>
+			<span slot="price">{c.price.toLocaleString('id-ID')}</span>
+			<span slot="unit">{c.unit}</span>
+		</ProductBox>
+	{/each}
+</div>
+<div class="mt-16">
+<select bind:value={limit} on:change={() => {
+  products = [];
+  page = 0;
+  hashMore = false;
+}}>
+  {#each [3,5,10,30,50] as n}
+    <option value={n}>{n}</option>
+  {/each}
+</select>
+<button disabled={hashMore} on:click={() => page = page+1}>More product...</button>
+</div>
+
+<style>
+	.div-product {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		gap: 20px;
+	}
+  .mt-16 {
+    margin-top: 16px;
+  }
+</style>
