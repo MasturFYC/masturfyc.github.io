f514b2b656da572130b79343e56cf65956a08904
MasturFyc <mastur.st12@gmail.com>
2023-10-17 14:52:30 +0700

    home

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -49,6 +49,9 @@
 			<div class="mt-2">
 				<a href="/pdam">Kartu PDAM</a>
 			</div>
+			<div class="mt-2">
+				<a href="/pt-gpm">PT. Galuh Persada Makmur</a>
+			</div>
 			<div class="fb-login-button" data-width="90" data-size="32" data-button-type="button" data-layout="" data-auto-logout-link="false" data-use-continue-as="false"></div>
 			<div>
 				<!-- svelte-ignore a11y-misplaced-scope -->
---
+++ src/routes/pt-gpm/+page.svelte
@@ -0,0 +1,27 @@
+<section>
+	<div class="title">PT. Galuh Persada Makmur</div>
+	<div>Download dokumen:</div>
+	<ol>
+		<li>
+			Logo
+			<div>
+				<a target="_blank" href="https://sapulidi.site/images/logo-gpm.svg"
+					><img
+						src="https://sapulidi.site/images/logo-gpm.svg"
+						alt="Logo PT. Galuh Persada Makmur"
+					/></a
+				>
+			</div>
+		</li>
+
+		<li><a  target="_blank" href="https://sapulidi.site/images/kop-surat.docx">Kop Surat</a></li>
+	</ol>
+</section>
+<style lang="scss">
+  ol {
+    margin-left: 16px;
+  }
+  li {
+    margin-top: 12px;
+  }
+</style>
