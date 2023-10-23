a09cd01aad85de54f2bd62229757b8faa3c03aa6
MasturFYC <mastur.st12@gmail.com>
2023-10-24 02:58:54 +0700

    home

--- docs/_app/immutable/chunks/fetch-api.25de0291.js
+++
@@ -1,1 +0,0 @@
-import{f as o}from"./index.01b9c5aa.js";const r="https://dapulidi.site/api",t=o(r,{mode:"cors"}).errorType("json");export{t as f};
---
+++ docs/_app/immutable/chunks/fetch-api.54bf1420.js
@@ -0,0 +1,1 @@
+import{f as o}from"./index.01b9c5aa.js";const s="https://sapulidi.site/api",t=o(s,{mode:"cors"}).errorType("json");export{t as f};
