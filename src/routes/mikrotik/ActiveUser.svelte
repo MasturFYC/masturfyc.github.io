23420446b83ed61781631edf7c1138636f8db1c2
MasturFYC <mastur.st12@gmail.com>
2024-01-04 01:28:10 +0700

    home

--- src/routes/mikrotik/ActiveUser.svelte
+++ src/routes/mikrotik/ActiveUser.svelte
@@ -7,7 +7,7 @@
 </script>
 
 {#each users as d, i (d.id)}
-<div class="columns is-gapless {innerWidth <= 640 ? 'p-2 has-background-light':i%2===1?'my-1 p-1 has-background-light':'my-1'}">
+<div class="columns is-gapless {innerWidth <= 640 ? 'p-2 has-background-light':i%2===1?'my-1 p-1 has-background-light':'px-1 my-1'}">
   <div class="column is-1">{d.id.substring(4, d.id.length)}</div>
   <div class="column is-2 has-text-weight-bold">{d.name}</div>
   <div class="column is-2">{d.comment}</div>
